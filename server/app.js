cedc249d717a10ac2546b12c4773b67f64ea4d37
Ben <ben@Bens-MacBook-Pro.local>
2020-07-06 14:34:48 +0100

    Init

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const graphqlHTTP = require("express-graphql")
+const mongoose = require("mongoose")
+
+const schema = require("./schema/schema")
+
+const app = express()
+
+mongoose.connect(
+  "mongodb+srv://Ben:Y1zQeEOaPYGwcl9l@gql-ninja-demo.dblxo.mongodb.net/GQL-Ninja-Demo?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+
+mongoose.connection.once("open", () => {
+  console.log("connected to the database")
+})
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+)
+
+app.listen(4000, () => {
+  console.log("Now listening for requests on port 4000")
+})
